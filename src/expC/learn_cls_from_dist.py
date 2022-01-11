9c2d4d58c2dc102411a4acad13a78dad6c623566
Aditya Chetan <t-adchetan@microsoft.com>
2022-01-11 12:47:58 +0000

    resolved bug with rsync

--- src/expC/learn_cls_from_dist.py
+++ src/expC/learn_cls_from_dist.py
@@ -507,7 +507,7 @@ def main(train, logdir, data, name, _log, _run, backup_dir):
 
     
     if backup_dir is not None:
-        bkup_cmd = "rsync -avzr {src} {dest}".format(logdir, backup_dir)
+        bkup_cmd = "rsync -avzr {src} {dest}".format(src=logdir, dest=backup_dir)
         bkup_cmd_list = bkup_cmd.split()
         sync_sanity_check_file = os.path.join(save_dir, "rsync_bkup_sanity_check_cout.txt")
 
