4033fd5d4143825c847f9bed4b7e6c7747437aa3
Aditya Chetan <t-adchetan@microsoft.com>
2022-04-22 12:02:44 +0000

    file changes for vm3 runs

--- src/expD/inpfn_ingredients.py
+++ src/expD/inpfn_ingredients.py
@@ -8,23 +8,18 @@ inpfn_ingredient = Ingredient('input_files')
 @inpfn_ingredient.config
 def inpfn_config():
 
-    from sacred import SETTINGS
-
-    SETTINGS.ENFORCE_KEYS_NO_EQUALS = False
-
     # proj_dir = "/azuredrive/deepimage/data1/t-achetan/adv_geom_dumps/dumps/expD_distlearner_against_adv_eg/rdm_concspheres/"
     # settings_to_analyze = {
     #     "rdm_concspheres_k100n100_noninfdist": ["1", "2"],
     #     "rdm_concspheres_k500n500_noninfdist": ["1", "2"]
     # }
     proj_dir = "/data/dumps/expC_dist_learner_for_adv_ex/rdm_concspheres_test/"
-    # settings_to_analyze = {
-    #     "rdm_concspheres_k50n500_noninfdist_moreoffmfld_advtraindebug_eps=1e-1": ["1"],
-    #     "rdm_concspheres_k50n500_noninfdist_moreoffmfld_advtraindebug_bs4096_eps=5e-2": ["1", "2"]
-    # }
-    settings_to_analyze = [
-        "rdm_concspheres_k50n500_noninfdist_moreoffmfld_advtraindebug_bs4096_eps=8e-2/1"
-    ]
+    settings_to_analyze = {
+        "rdm_concspheres_k50n500_noninfdist_moreoffmfldv3_bs4096_highmn20": ["1"]
+    }
+    # settings_to_analyze = [
+    #     "rdm_concspheres_k50n500_noninfdist_moreoffmfld_advtraindebug_bs4096_eps=8e-2/1"
+    # ]
     settings_fn = None
     if settings_fn is not None:
         with open(settings_fn) as f:
