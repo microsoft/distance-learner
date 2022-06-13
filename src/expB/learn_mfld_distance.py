ccdce78d6fc90a85e647adcfbfa841f5aa1a2a67
Aditya Chetan <t-adchetan@microsoft.com>
2022-06-13 13:00:16 +0000

    minor fix in training script for MNIST

--- src/expB/learn_mfld_distance.py
+++ src/expB/learn_mfld_distance.py
@@ -231,7 +231,7 @@ def train(model, optimizer, loss_func, dataloaders, device, save_dir, scheduler,
             writer.add_text(phase + "/data/params", str({i: data_vars[i] for i in data_vars if i in capture_attrs}))
         
 
-        writer.add_graph(model, dataloaders["train"].dataset.all_points[:dataloaders["train"].batch_size])
+        writer.add_graph(model, dataloaders["train"].dataset.normed_all_points[:dataloaders["train"].batch_size])
 
         
 
