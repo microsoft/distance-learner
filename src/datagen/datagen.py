9b8009e46996f139194cf9ec44763370e03dd0e4
Aditya Chetan <achetan40@gmail.com>
2022-07-02 02:21:01 +0530

    Update datagen.py

--- src/datagen/datagen.py
+++ src/datagen/datagen.py
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 from datagen.synthetic.single import sphere, swissroll
 from datagen.synthetic.multiple import intertwinedswissrolls, concentricspheres, wellseparatedspheres
 from datagen.real import mnist
