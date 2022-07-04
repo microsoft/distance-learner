9546df350498c739ba75b316a5e33f04d4807c31
Aditya Chetan <achetan40@gmail.com>
2022-07-05 00:55:26 +0530

    Update run_ws_m1n2.sh

--- src/reproduce/run_ws_m1n2.sh
+++ src/reproduce/run_ws_m1n2.sh
@@ -4,8 +4,8 @@ cd ../expC
 
 python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=1e-5 batch_size=512 debug=False data.mtype=inf-ws-spheres tgtname=normed_actual_distances loss_func=std_mse \
  data.data_tag=rdm_wsspheres_m1n2 \
- data.logdir="./data" \
- data.backup_dir="./data" \
+ data.logdir="../data" \
+ data.backup_dir="../data" \
  data.data_params.train.k=2 \
  data.data_params.train.n=2 \
  data.data_params.train.N=1500000 \
@@ -24,8 +24,8 @@ python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=1
 
  python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=1e-5 batch_size=512 debug=False data.mtype=inf-ws-spheres tgtname=normed_actual_distances loss_func=std_mse \
  data.data_tag=rdm_wsspheres_m1n2 \
- data.logdir="./data" \
- data.backup_dir="./data" \
+ data.logdir="../data" \
+ data.backup_dir="../data" \
  data.data_params.train.k=2 \
  data.data_params.train.n=2 \
  data.data_params.train.N=1500000 \
