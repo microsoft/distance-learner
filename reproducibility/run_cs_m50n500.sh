8783a1f3428b92c24250caeba77967fb084e3bad
Aditya Chetan <achetan40@gmail.com>
2022-07-04 17:49:51 +0530

    Update run_cs_m50n500.sh

--- reproducibility/run_cs_m50n500.sh
+++ reproducibility/run_cs_m50n500.sh
@@ -100,4 +100,6 @@ python3 learn_cls_from_dist.py with cuda=3 num_epochs=1000 cooldown=700 warmup=1
  data.generate=False \
  task=clf
 
+cd ../expD
+
 python3 get_attack_perf.py with debug=False "attack.atk_routine=['my']" input_files.settings_type=list input_files.proj_dir="./data/rdm_concspheres_test/" dump_dir="./data/rdm_concspheres_test/attack_perfs_on_runs" "input_files.settings_to_analyze=['rdm_concspheres_m50n500/1', 'rdm_concspheres_m50n500/2', 'rdm_concspheres_m50n500/3', 'rdm_concspheres_m50n500/4']"
