7a29dd550cfbcf3f9d5569b3f224d942e9244191
Aditya Chetan <achetan40@gmail.com>
2022-07-05 00:51:43 +0530

    Update run_cs_m1n50.sh

--- src/reproduce/run_cs_m1n50.sh
+++ src/reproduce/run_cs_m1n50.sh
@@ -4,7 +4,7 @@
 cd ../expC
 
 # train distlearner
-python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=1.5e-5 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-conc-spheres data.logdir="./data" \
+python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=1.5e-5 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-conc-spheres data.logdir="../data" \
  data.data_tag=rdm_concspheres_m1n50 \
  data.data_params.train.N=1500000 \
  data.data_params.train.num_neg=500000 \
@@ -27,7 +27,7 @@ python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=1
  task=regression
 
 # train stdclf
-python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=8e-5 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-conc-spheres data.logdir="./data" \
+python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=8e-5 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-conc-spheres data.logdir="../data" \
  data.data_tag=rdm_concspheres_m1n50 \
  data.data_params.train.N=1500000 \
  data.data_params.train.num_neg=500000 \
