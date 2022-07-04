30e053e8510a0fd9233e577c38e1622e1a7e6540
Aditya Chetan <achetan40@gmail.com>
2022-07-05 00:55:02 +0530

    Update run_sw_m1n500.sh

--- src/reproduce/run_sw_m1n500.sh
+++ src/reproduce/run_sw_m1n500.sh
@@ -4,8 +4,8 @@ cd ../expC
 
 python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=1e-6 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-ittw-swissrolls \
  data.data_tag=rdm_swrolls_m1n500 \
- data.logdir="./data" \
- data.backup_dir="./data" \
+ data.logdir="../data" \
+ data.backup_dir="../data" \
  data.data_params.train.k=2 \
  data.data_params.train.n=500 \
  data.data_params.train.N=150000 \
@@ -20,8 +20,8 @@ python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=1
 
 python3 learn_cls_from_dist.py with cuda=0 num_epochs=1000 cooldown=700 warmup=10 lr=8e-5 batch_size=4096 debug=False loss_func=std_mse tgtname=normed_actual_distances data.mtype=inf-ittw-swissrolls \
  data.data_tag=rdm_swrolls_m1n500 \
- data.logdir="./data" \
- data.backup_dir="./data" \
+ data.logdir="../data" \
+ data.backup_dir="../data" \
  data.data_params.train.k=2 \
  data.data_params.train.n=500 \
  data.data_params.train.N=150000 \
